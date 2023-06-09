dd8d461d3a25cea9df4fdf67dd1cf6f633f1e549
subaek26 <subaek@davidson.edu>
2023-06-09 16:09:26 -0400

    Initial Commit

---
+++ P2P_chatBox/P2P_chatBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  P2P_chatBoxApp.swift
+//  P2P_chatBox
+//
+//  Created by Sunny Baek on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct P2P_chatBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
