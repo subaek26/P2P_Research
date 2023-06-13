7832854288d3c7f09adfd949a1e7a2942ebe33ef
subaek26 <subaek@davidson.edu>
2023-06-13 14:50:38 -0400

    Login page

---
+++ P2P_chatBox/LoginPageView.swift
@@ -0,0 +1,71 @@
+//
+//  LoginPageView.swift
+//  P2P_chatBox
+//
+//  Created by Sunny Baek on 6/13/23.
+//
+
+import SwiftUI
+
+struct LoginPageView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername: Float = 0
+    @State private var wrongPassword: Float  = 0
+    @State private var showingLoginScreen = false
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                        }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }.navigationBarHidden(true)
+        }
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() != "" {
+            wrongUsername = 0
+            showingLoginScreen = true
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPageView()
+    }
+}
