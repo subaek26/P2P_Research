f297ade9cc97935c8d29e32fbb1570e410316159
subaek26 <subaek@davidson.edu>
2023-06-12 22:54:33 -0400

    ChatModel updated

---
+++ P2P_chatBox/ChatModel.swift
@@ -0,0 +1,55 @@
+//
+//  ChatModel.swift
+//  P2P_chatBox
+//
+//  Created by Sunny Baek on 6/12/23.
+//
+
+import Foundation
+
+struct Chat: Identifiable {
+    var id: UUID { person.id }
+    let person: Person
+    var messages: [Message]
+    var hasUnreadMessage = false
+}
+
+struct Person: Identifiable {
+    let id = UUID()
+    let name: String
+    let imgString: String
+}
+
+struct Message: Identifiable {
+    
+    enum MessageType{
+        case Sent, Received
+    }
+    
+    let id = UUID()
+    let date: Date
+    let text: String
+    let type: MessageType
+    
+    init(_ text: String, type: MessageType, date: Date) {
+        self.date = date
+        self.type = type
+        self.text = text
+    }
+    init(_ text: String, type: MessageType) {
+        self.init(text, type: type, date: Date())
+    }
+}
+
+extension Chat {
+    
+    static let sampleChat = [
+        Chat(person: Person(name: "Wendy", imgString: "img1"), messages: [
+            Message("Hey Wendy", type: .Sent, date: Date(timeIntervalSinceNow: -86400 * 3)),
+            Message("I am jsut developing an WhatsApp Clone App", type: .Sent, date: Date(timeIntervalSinceNow: -86400 * 3)),
+                    ],
+                    hasUnreadMessage: true)
+            ]
+    
+}
+
